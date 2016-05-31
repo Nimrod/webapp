978845469779e4f8c9d4a30fbe40bc7746e5bd06
nimrod <nimrodbaron1@gmail.com>
2016-05-31 22:45:29 +0300

    31.5.16 Created a locale storage object. In it created a tab name object.  After URL validation in the for loop add name and URL values to it.

--- js/script.js
+++ js/script.js
@@ -1,107 +1,121 @@
 /**
  * Created by Admin on 26/05/2016.
  */
-$("#showForm").parent().click(function(){
-   if ($(".form").hasClass("display-block")){
-       $(".form").removeClass("display-block")
-       $("#showForm").parent().removeAttr("style");
+(function () {
+    //create locale storage object
+    var localStorage = {
 
-   }
-    else {
-       $(".form").addClass("display-block");
+    };
 
-       $("#showForm").parent().css("background-color", "#FEFEFE");
-   }
+    $("#showForm").parent().click(function(){
+       if($("#showForm").hasClass("animation")) {
+                $("#showForm").removeClass("animation");
+                //debugger;
+            }
 
-}
-);
+       if ($(".form").hasClass("display-block")){
+           $(".form").removeClass("display-block");
+           $("#showForm").parent().removeAttr("style");
+           $("#showForm").addClass("animation");
+       }
+        else {
+           $(".form").addClass("display-block");
+           $("#showForm").parent().css("background-color", "#FEFEFE");
+           $("#showForm").addClass("animation");
+       }
+    });
 
 
+    //when clicking the submit button:
+    $("#submitBtn").click(function(){
+        var tabName = $(this).parent().parent().parent().attr("id")
+        localStorage["tabName"] = {};
 
+        // create two arrays, 1. name fields 2. url fields,
+        // both name and url field in the same pair has the same index in their array.
+        var nameArr = $(".form").find(".name").toArray();
+        var urlArr = $(".form").find(".siteUrl").toArray();
+        //var siteUrl;
 
-//when clicking the submit button:
-$("#submitBtn").click(function(){
+        // loop through name array
+        function checkForm() {
+            //create an empty array of the fields with wrong url
+            var wrongUrl = [];
+            //create var test to check if all url fields ok, take off text bubble
+            var bubbleTest = "";
 
-    // create two arrays, 1. name fields 2. url fields,
-    // both name and url field in the same pair has the same index in their array.
-    var nameArr = $(".form").find(".name").toArray();
-    var urlArr = $(".form").find(".siteUrl").toArray();
-    //var siteUrl;
+            //loop through fields
+            for (var i=0; i<nameArr.length; i++) {
+                //name + url fields
+                var thisNameField = nameArr[i],
+                    thisUrlField = urlArr[i],
+                //name + url fields value
+                    nameText = $(nameArr[i]).val(),
+                    urlText = $(urlArr[i]).val();
 
-    // loop through name array
-    function checkForm() {
-        //create an empty array of the fields with wrong url
-        var wrongUrl = [];
-        //create var test to check if all url fields ok, take off text bubble
-        var bubbleTest = "";
+                function testUrl() {
+                    //check if ur is valid and return answer
+                    function validUrl() {
+                        var re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+                        return re.test(urlText);
+                    }
 
-        //loop through fields
-        for (var i=0; i<nameArr.length; i++) {
-            //name + url fields
-            var thisNameField = nameArr[i],
-                thisUrlField = urlArr[i],
-            //name + url fields value
-                nameText = $(nameArr[i]).val(),
-                urlText = $(urlArr[i]).val();
+                    //if valid add name and URL values to localeStorage object + remove red border
+                    if (validUrl() === true) {
+                        localStorage["tabName"]["nameField" +"#" +[i]] = nameText;
+                        localStorage["tabName"]["urlField" +"#" +[i]] = urlText;
+                        $(thisUrlField).removeClass("redBorder");
+                    }
 
-            function testUrl() {
-                //check if ur is valid and return answer
-                function validUrl() {
-                    var re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
-                    return re.test(urlText);
+                    //if not valid add red border, push field into wrongUrl array, change bubbleTest to true.
+                    else {
+                        $(thisUrlField).addClass("redBorder");
+                        wrongUrl.push(thisUrlField);
+                        bubbleTest = true;
+                    }
                 }
 
-                //if valid remove red border
-                if (validUrl() === true) {
+                //if both name field and url field empty remove red border
+                if (( nameText === "") && (urlText === "" )) {
+                    $(thisNameField).removeClass("redBorder");
                     $(thisUrlField).removeClass("redBorder");
                 }
 
-                //if not valid add red border, push field into wrongUrl array, change bubbleTest to true.
-                else {
-                    $(thisUrlField).addClass("redBorder");
-                    wrongUrl.push(thisUrlField);
-                    bubbleTest = true;
+                //if both name field and url field has value remove red border but check if url is valid.
+                if (( nameText !== "") && (urlText !== "" )){
+                    $(thisNameField).removeClass("redBorder");
+                    testUrl();
+                }
+
+                // if name field has value and url field empty add red border and placeholder
+                if (( nameText !== "") && (urlText === "" )) {
+                    $(urlArr[i]).attr("placeholder", "Please enter a valid URL").addClass("redBorder");
+                }
+
+                // if url field has value and name field empty add red border and placeholder and check if url is valid.
+                if (( nameText === "") && (urlText !== "" )) {
+                    testUrl();
+                    $(nameArr[i]).attr("placeholder", "Please enter a name").addClass("redBorder");
                 }
             }
-
-            //if both name field and url field empty remove red border
-            if (( nameText === "") && (urlText === "" )) {
-                $(thisNameField).removeClass("redBorder");
-                $(thisUrlField).removeClass("redBorder");
+            //if bubble test is empty remove all text bubbles
+            if (bubbleTest === ""){
+                $(".bubble").fadeOut(50);
             }
-
-            //if both name field and url field has value remove red border but check if url is valid.
-            if (( nameText !== "") && (urlText !== "" )){
-                $(thisNameField).removeClass("redBorder");
-                testUrl();
-            }
-
-            // if name field has value and url field empty add red border and placeholder
-            if (( nameText !== "") && (urlText === "" )) {
-                $(urlArr[i]).attr("placeholder", "Please enter a valid URL").addClass("redBorder");
-            }
-
-            // if url field has value and name field empty add red border and placeholder and check if url is valid.
-            if (( nameText === "") && (urlText !== "" )) {
-                testUrl();
-                $(nameArr[i]).attr("placeholder", "Please enter a name").addClass("redBorder");
+            //else put the bubble under first wrong url field
+            else {
+                var urlBubble = wrongUrl[0];
+                var position = $(urlBubble).position();
+                $(".bubble").css("top", position.top + 18);
+                $(".bubble").css("left", position.left + 4);
+                $(".bubble").show();
             }
         }
-        //if bubble test is empty remove all text bubbles
-        if (bubbleTest === ""){
-            $(".bubble").fadeOut(50);
-        }
-        //else put the bubble under first wrong url field
-        else {
-            var urlBubble = wrongUrl[0];
-            var position = $(urlBubble).position();
-            $(".bubble").css("top", position.top + 18);
-            $(".bubble").css("left", position.left + 4);
-            $(".bubble").show();
-        }
-    }
-    // on submit or blur of one of fields check form
-    checkForm();
-    $(".input").on("blur", checkForm);
+        // on submit or blur of one of fields check form
+        checkForm();
+        $(".input").on("blur", checkForm);
+        console.log(localStorage);
 
-});
+
+    });
+})();
